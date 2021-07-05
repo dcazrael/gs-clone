e5f9245279acffaac76f263b0fcd08566e0b4476
dcazrael <msachdev@live.de>
2021-07-05 12:20:15 +0900

    initial next.js setup including tailwind

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
-import 'tailwindcss/tailwind.css'
+import { ThemeProvider } from 'next-themes';
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider
+      attribute='class'
+      storageKey='nightwind-mode'
+      defaultTheme='system'
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,13 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    nightwind: {
+      typography: true,
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
-  plugins: [],
-}
+  plugins: [require('@tailwindcss/typography')],
+};
